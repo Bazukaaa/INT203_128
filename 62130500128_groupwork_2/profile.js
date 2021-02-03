82ab6a99764a212d76d10cdbc99d1f151d4964ea
Poramet Ly <60100309+Bazukaaa@users.noreply.github.com>
2021-02-03 12:03:25 +0700

    [Upload] add work 2

---
+++ 62130500128_groupwork_2/profile.js
@@ -0,0 +1,17 @@
+    const app = {
+        data() {
+            return {
+                // msg: 'Hello, Vue3',
+                firstname: 'Poramet',
+                
+            // firstname: 'Pornpipat',
+            lastname:'MKMS',
+            career:'Gamer,Programmer',
+            image:'./images/profile2.jpg',
+            url: 'https://www.facebook.com/ly.poramat/'
+                
+            }
+        }
+
+    }
+    Vue.createApp(app).mount('#app')
